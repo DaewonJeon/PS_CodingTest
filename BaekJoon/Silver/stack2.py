ca79311d49f8353eb79e8c2d82a8a9fabc828546
전대원 <monst7943@gmail.com>
2025-12-23 21:26:32 +0900

    stack_algo

---
+++ BaekJoon/Silver/stack2.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+stack=[]
+
+for i in range(n):
+    a = input().split()
+
+    if a[0] == "1":
+        stack.append(a[1])
+    
+    elif a[0] == "2":
+        if stack:
+            print(stack[-1])
+            stack.pop()
+        else:
+            print("-1")
+
+    elif a[0] == "3":
+        print(len(stack))
+
+    elif a[0] == "4":
+        if not stack:
+            print("1")
+        else:
+            print("0")
+
+    elif a[0] == "5":
+        if stack:
+            print(stack[-1])
+        else:
+            print("-1")    
+
+    
+
+
