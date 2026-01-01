9ae47fb6e96a3c0559462e09c6371fa2b40c8857
전대원 <monst7943@gmail.com>
2026-01-02 02:24:13 +0900

    심화2

--- BaekJoon/Silver/GomGom.py
+++ BaekJoon/Silver/GomGom.py
@@ -1,6 +1,6 @@
 import sys             # 심화2 - 인사성 밝은 곰곰이 set()
-input = sys.stdin.readline
-
+input = sys.stdin.readline   # set - 중복없는 자료 (중복 X , 순서 X, 빠른 검색)
+                             # set 비우기 - .clear() 리스트,세트,딕셔너리 모두 가능함 
 n = int(input())
 user = set()
 count = 0
