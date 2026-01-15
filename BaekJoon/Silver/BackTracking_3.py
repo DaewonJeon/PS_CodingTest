49c67572c6647176f1a6fc1772ca400e1d344795
전대원 <monst7943@gmail.com>
2026-01-15 13:50:18 +0900

    백트래킹_알고리즘_완료

---
+++ BaekJoon/Silver/BackTracking_3.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+result = []
+
+
+def dfs():
+    if len(result) == m:
+        print(' '.join(map(str, result)))
+        return
+    
+    for i in range(1, n+1):
+
+        result.append(i)
+        
+        dfs()
+
+        result.pop()
+
+dfs()
---
+++ BaekJoon/Silver/BackTracking_4.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+result = []
+
+
+def dfs(start):
+    if len(result) == m:
+        print(' '.join(map(str, result)))
+        return
+    
+    for i in range(start, n+1):
+
+        result.append(i)
+        
+        dfs(i)
+
+        result.pop()
+
+dfs(1)
